b7c2b924a7e403d6ae4963f2f38dca6964bfecb3
deek-mei <129723601+deek-mei@users.noreply.github.com>
2023-04-12 03:14:32 -0400

    Added CSS

--- age-calculator-app-main/age-calculator-app/src/App.jsx
+++ age-calculator-app-main/age-calculator-app/src/App.jsx
@@ -56,6 +56,8 @@ function App() {
   }
 
   function validateEntry() {
+    console.log("ran")
+
     let dayInt = parseInt(day, 10);
     let thirtyOneDaysMonths = [1, 3, 5, 7, 8, 10, 12];
     let monthInt = parseInt(month, 10);
@@ -69,17 +71,14 @@ function App() {
     ) { setDayError(true); setAge(reset) } else { setDayError(false) }
 
     if (month === "" || monthInt > 12 || monthInt === 0) { setMonthError(true); setAge(reset) } else { setMonthError(false) }
-    if (year === "" || yearInt > 2023 || yearInt < 1900) { setYearError(true); setAge(reset) } else { setYearError(false) }
+    if (year === "" || yearInt > 2023 || yearInt < 0) { setYearError(true); setAge(reset) } else { setYearError(false) }
 
     const calcDay = (parseInt(currentDay, 10) - parseInt(day, 10))
     const calcMonth = (parseInt(currentMonth, 10) - parseInt(month, 10));
-
-    if ((calcDay < 0 || calcMonth < 0) && currentYear === 2023) {
+    if ((calcDay < 0 || calcMonth < 0) && parseInt(year, 10) === 2023) {
       setDayError(true);
       setMonthError(true);
-      setYearError(true)
-      console.log(yearError)
-      return
+      setYearError(true);
     }
   }
 
@@ -90,9 +89,9 @@ function App() {
 
     setAge({
       ...age,
-      month: calcMonth,
-      day: calcDay,
-      year: calcYear
+      month: Math.abs(calcMonth),
+      day: Math.abs(calcDay),
+      year: Math.abs(calcYear)
 
     })
     console.log(age)
@@ -105,42 +104,45 @@ function App() {
     }
   }
 
+
   return (
     <>
 
       <div className='inputBar'>
-        <label htmlFor='day-input'>DAY</label>
-        <input
-          className='input'
-          id='day-input'
-          type='text'
-          placeholder='DD'
-          maxlength="2"
-          value={day}
-          onChange={handleChange("day")} />
-        {dayError && <p>Must be a valid day</p>}
-
+        <div className='day-div' style={{ color: dayError ? 'hsl(0, 100 %, 67 %)' : 'black' }}>
+          <label style={{ color: dayError ? 'hsl(0, 100 %, 67 %)' : 'black' }} htmlFor='day-input'>DAY</label>
+          <input
+            className='input'
+            id='day-input'
+            type='text'
+            placeholder='DD'
+            maxlength="2"
+            value={day}
+            onChange={handleChange("day")} />
+          {dayError && <p className="error">Must be a valid day</p>}
+        </div>
         <label htmlFor='month-input'>MONTH</label>
         <input className='input' id='month-input' type='text' placeholder='MM' maxlength="2"
           value={month}
           onChange={handleChange("month")}
         ></input>
-        {monthError && <p>Must be a valid month</p>}
+        {monthError && <p className="error">Must be a valid month</p>}
 
         <label htmlFor='year-input'>YEAR</label>
         <input className='input' id='year-input' type='text' placeholder='YYYY' maxlength="4"
           value={year}
           onChange={handleChange("year")}
         ></input>
-        {yearError && <p>Must be a valid year</p>}
-
-        <button onClick={handleButtonClick}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44">
-            <g fill="none" stroke="#FFF" stroke-width="2">
-              <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
-            </g>
-          </svg>
-        </button>
+        {yearError && <p className="error">Must be a valid year</p>}
+        <div className='button-wrapper'>
+          <button onClick={handleButtonClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44">
+              <g fill="none" stroke="#FFF" stroke-width="2">
+                <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
+              </g>
+            </svg>
+          </button>
+        </div>
       </div>
 
       <div>
@@ -148,17 +150,19 @@ function App() {
 
 
       <div>
-        <div>
-          <h1>
-            {age.year === "" && age.year !== 0 ? <p>--</p> : <p>{age.year}</p>}years</h1>
+        <div className='age-group'>
+          <h1 style={{ display: "flex", alignItems: "center" }}>
+            {age.year === "" && age.year !== 0 ? <p className='blank-age'>-- </p> : <p>{age.year}</p>}years</h1>
+        </div >
+        <div className='age-group'>
+          <h1 style={{ display: "flex", alignItems: "center" }}>
+            {age.month === "" && age.month !== 0 ? <p className='blank-age'>-- </p> : <p>{age.month}</p>}months</h1>
         </div>
-        <div>
-          <h1>{age.month === "" && age.month !== 0 ? <p>--</p> : <p>{age.month}</p>}months</h1>
+        <div className='age-group'>
+          <h1 style={{ display: "flex", alignItems: "center" }}>
+            {age.day === "" && age.day !== 0 ? <p className='blank-age'>-- </p> : <p>{age.day}</p>}days</h1>
         </div>
-        <div>
-          <h1>{age.day === "" && age.day !== 0 ? <p>--</p> : <p>{age.day}</p>}days</h1>
-        </div>
-      </div>
+      </div >
     </>
   )
 }
